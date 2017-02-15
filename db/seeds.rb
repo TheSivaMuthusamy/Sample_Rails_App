ae97970088df39d624f8e088a0180b9606f964df
Siva Muthusamy <s.b.muthusamy@gmail.com>
2017-02-14 19:20:33 -0500

    Prepared for Heroku Deplyoment

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,7 @@ User.create!(name:  "Example User",
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
-  User.create!(name:  name
+  User.create!(name:  name,
                email: email,
                password:              password,
                password_confirmation: password,
